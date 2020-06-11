d1eb20a4953915229da3c6306cfc938f9b72ddbc
Vladimir Shishov <wladimir.shishov@gmail.com>
2020-06-11 10:56:41 +0300

    fix read options. create decsritprion

--- README.md
+++ README.md
@@ -6,3 +6,22 @@
 
 ## bayan
 Duplicate file search utility
+
+### Options:
+```bash
+  -i [ --include    ] Include paths for scaning (required)
+  -e [ --exclude    ] Exclude paths from scaning (optional, by default is not set)
+  -l [ --level      ] Scan nesting level (optional, by default is 0 - without nested)
+  -s [ --min-size   ] Minimum file size in bytes for scaning (optional, by default is 1)
+  -m [ --masks      ] Masks for file names allowed for comparison (optional, by default is not set)
+  -b [ --block-size ] Block size in bytes for reading file (optional, by default is 1024)
+  -a [ --algorithm  ] Hash algorithm: crc32 or md5 (optional, by default is crc32)
+  -h [ --help       ] Show help
+```
+
+### Examples: 
+**long options:**
+`bayan --include /home --exclude /~/temp --level=3 --min-size=256 --masks *.cpp *.txt --block-size=1024 --algorithm=crc32`
+
+**short options:**
+`bayan -i /home -e/~/temp -l 3 -s256 -m*.cpp *.txt -b1024 -acrc32`
--- src/options.cpp
+++ src/options.cpp
@@ -1,8 +1,17 @@
 #include "options.h"
 
 #include <iostream>
+#include <exception>
 
 namespace Otus {
+
+class InvalidOption: public std::logic_error 
+{
+public:
+  explicit InvalidOption(const std::string &input)
+    : std::logic_error(input) 
+  { }
+};
 
 OptionsParser::OptionsParser()
   : m_Description("Options")
@@ -10,12 +19,12 @@ OptionsParser::OptionsParser()
   m_Description.add_options()
     ("include,i",     bpo::value<std::vector<bfs::path>>(), "Include paths for scaning")
     ("exclude,e",     bpo::value<std::vector<bfs::path>>(), "Exclude paths from scaning")
-    ("level,l",       bpo::value<size_t>(),                 "Scan nesting level")
-    ("min-size,ms",   bpo::value<size_t>(),                 "Minimum file size in bytes for scaning, bytes")
-    ("masks,m",       bpo::value<std::string>(),            "Masks for file names allowed for comparison")
-    ("block-size,bs", bpo::value<size_t>(),                 "Block size for reading file, bytes")
+    ("level,l",       bpo::value<long>(),                   "Scan nesting level, 0 - without nested")
+    ("min-size,s",    bpo::value<long>(),                   "Minimum file size in bytes for scaning")
+    ("masks,m",       bpo::value<std::vector<std::string>>(),            "Masks for file names allowed for comparison")
+    ("block-size,b",  bpo::value<long>(),                   "Block size in bytes for reading file")
     ("algorithm,a",   bpo::value<std::string>(),            "Hash algorithm: crc32 or md5")
-    ("help,h",        "show help")  
+    ("help,h",        "Show help")  
   ;
 }
 
@@ -34,18 +43,64 @@ OptionsParser::OptionsResult OptionsParser::GetOptions(int argc, char** argv)
 
     if (m_Variables.count("include")) {
       options.includePaths = m_Variables["include"].as<std::vector<bfs::path>>();
+      for (auto& path : options.includePaths) {
+        if (path.is_relative()) {
+          path = bfs::canonical(path);
+        }
+      }
+    }
+    else {
+      throw InvalidOption("Include scan paths not defined");
     }
 
     if (m_Variables.count("exclude")) {
       options.excludePaths = m_Variables["exclude"].as<std::vector<bfs::path>>();
+      for (auto& path : options.excludePaths) {
+        if (path.is_relative()) {
+          path = bfs::canonical(path);
+        }
+      }
     }
 
-    // if (nMinFileSize < 1) {
-    // std::cerr << "Error: Minimum file size must be greater than 1!" << std::endl;
-    // }
+    if (m_Variables.count("level")) {
+      long nLevel = m_Variables["level"].as<long>();
+      if (nLevel < 0) {
+        throw InvalidOption("Level scaning must be integer");
+      }
+      options.levelScannig = static_cast<std::size_t>(nLevel);
+    }
+
+    if (m_Variables.count("min-size")) {
+      long nMinSize = m_Variables["min-size"].as<long>();
+      if (nMinSize < 1) {
+        throw InvalidOption("Minimum file size must be greater then 1");
+      }
+      options.minFileSize = static_cast<std::size_t>(nMinSize);
+    }
+
+    if (m_Variables.count("masks")) {
+      options.masks = m_Variables["masks"].as<std::vector<std::string>>();
+    }
+
+    if (m_Variables.count("block-size")) {
+      long nBlockSize = m_Variables["block-size"].as<long>();
+      if (nBlockSize < 1) {
+        throw InvalidOption("Minimum file size must be greater then 1");
+      }
+      options.blockSize = static_cast<std::size_t>(nBlockSize);
+    }
+
+    if (m_Variables.count("algorithm")) {
+      std::string strHash = m_Variables["algorithm"].as<std::string>();
+      if (strHash != "crc32" && strHash != "md5") {
+        throw InvalidOption("The hash algorithm is invalid");
+      }
+    }
+
     return options;
   }
   catch(std::exception& ex) {
+    std::cout << "Error: " << ex.what() << std::endl << std::endl;
     std::cout << m_Description << std::endl;
     return OptionsResult();
   }  
