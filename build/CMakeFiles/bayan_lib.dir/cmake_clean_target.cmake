cde6cc0414531c4d79a3d47771523acd5821c56e
Vladimir Shishov <wladimir.shishov@gmail.com>
2020-06-15 23:58:16 +0300

    delete artifacts

--- build/CMakeFiles/bayan_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libbayan_lib.a"
-)
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bob/cpp_otus/projects/otus_cpp_09
-# Build directory: /home/bob/cpp_otus/projects/otus_cpp_09/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(gtest_bayan "gtest_bayan")
-subdirs("gtest")
